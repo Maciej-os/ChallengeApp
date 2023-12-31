c7bcfc8d5493842c62363950d8257fad4d06a469
Maciej-os <ostrowski86@gmail.com>
2023-12-31 16:49:10 +0100

    Zadanie na dzien 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,33 @@
+﻿string name = "Ewa";
+bool woman = true;
+byte age = 31;
+
+
+if (woman)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
+    else
+    {
+        Console.WriteLine("Brak klasyfikacji");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Brak klasyfikacji");
+    }
+
+}
+
