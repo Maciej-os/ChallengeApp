98778e91cbf4bc5be5b56230438a670c9201d4f6
Maciej-os <ostrowski86@gmail.com>
2024-01-14 01:18:20 +0100

    Zadanie, dzien #6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        //public static string GameName = "Giereczka";
+
+        private List<int> score = new List<int>();
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get;  }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,33 +1,45 @@
-﻿string name = "Ewa";
-bool woman = true;
-byte age = 31;
+﻿using ChallengeApp;
+
+Employee user1 = new Employee("Adam", "Kowal", 18);
+Employee user2 = new Employee("Zuzia", "Nowak", 23);
+Employee user3 = new Employee("Monika", "Myk", 44);
 
 
-if (woman)
+Console.WriteLine(user1.Name + " " + user1.Surname + " " + user1.Age);
+Console.WriteLine(user2.Name + " " + user2.Surname + " " + user2.Age);
+Console.WriteLine(user3.Name + " " + user3.Surname + " " + user3.Age);
+
+
+user1.AddScore(1);
+user1.AddScore(1);
+user1.AddScore(1);
+user1.AddScore(1);
+user1.AddScore(1);
+
+user2.AddScore(2);
+user2.AddScore(2);
+user2.AddScore(2);
+user2.AddScore(2);
+user2.AddScore(2);
+
+user3.AddScore(3);
+user3.AddScore(3);
+user3.AddScore(3);
+user3.AddScore(3);
+user3.AddScore(3);
+
+if ((user3.Result >= user2.Result) && (user3.Result >= user1.Result))
 {
-    if (age < 30)
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
-    else if (name == "Ewa" && age == 30)
-    {
-        Console.WriteLine("Ewa, lat 30");
-    }
-    else
-    {
-        Console.WriteLine("Brak klasyfikacji");
-    }
+    Console.WriteLine(user3.Name + " " + user3.Result);
 }
-else
-{
-    if (age < 18)
-    {
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    }
-    else
-    {
-        Console.WriteLine("Brak klasyfikacji");
-    }
 
+if ((user2.Result >= user1.Result) && (user2.Result >= user3.Result))
+{
+    Console.WriteLine(user2.Name + " " + user2.Result);
+}
+
+if ((user1.Result >= user2.Result) && (user1.Result >= user3.Result))
+{
+    Console.WriteLine(user1.Name + " " + user1.Result);
 }
 
