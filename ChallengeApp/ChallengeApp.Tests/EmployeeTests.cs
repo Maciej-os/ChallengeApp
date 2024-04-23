83df85116473eb2b989f2d9c3bee1d26680498d6
Maciej-os <ostrowski86@gmail.com>
2024-04-23 13:36:09 +0200

    Testy-Dzien#7

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,63 @@
+
+
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+        
+        [Test]
+        public void WhenEmployeeCollectScoresPlusOrMinus_ShouldReturnCorrectResult()
+        {
+            // arrange
+            var employee = new Employee("Rafa³", "Kaczka12!", 34);
+            employee.AddScore(5);
+            employee.AddScore(5);
+            employee.AddScore(5);
+            employee.AddScore(-10);
+            employee.AddScore(1);
+
+            //act
+
+            var result = employee.Result;
+
+            //asert
+            Assert.AreEqual(6, result);
+        }
+        [Test]
+        public void WhenEmployeeCollectOnlyScoresMinus_ShouldReturnCorrectResult()
+        {
+            // arrange
+            var employee = new Employee("Rafa³", "Kaczka12!", 34);
+            employee.AddScore(-5);
+            employee.AddScore(-5);
+            employee.AddScore(-5);
+            employee.AddScore(-10);
+            employee.AddScore(-1);
+
+            //act
+
+            var result = employee.Result;
+
+            //asert
+            Assert.AreEqual(-26, result);
+        }
+        [Test]
+        public void WhenEmployeeCollectOnlyScoresPlus_ShouldReturnCorrectResult()
+        {
+            // arrange
+            var employee = new Employee("Rafa³", "Kaczka12!", 34);
+            employee.AddScore(5);
+            employee.AddScore(5);
+            employee.AddScore(5);
+            employee.AddScore(10);
+            employee.AddScore(1);
+
+            //act
+
+            var result = employee.Result;
+
+            //asert
+            Assert.AreEqual(26, result);
+        }
+    }
+}
